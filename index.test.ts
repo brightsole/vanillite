6d87e37c3fac4bbf6bfef55ccfe1129f1252581e
Drew Showalter <maelstroswim@gmail.com>
2020-03-22 17:06:29 +1100

    :recycle::rotating_light: major refactor and main functionality tests.

--- index.test.ts
+++ index.test.ts
@@ -1,7 +1,99 @@
 import test from 'ava';
+import { performance } from 'perf_hooks';
+import * as localForage from 'localforage';
 // eslint-disable-next-line import/extensions
 import Vanillite from './index';
 
+const getActionTime = async (
+  testedFunction: () => Promise<any>
+): Promise<number> => {
+  const startTime = performance.now();
+  await testedFunction();
+  const finalTime = performance.now();
+
+  return finalTime - startTime;
+};
+const sleep = ms => new Promise(r => setTimeout(r, ms));
+
+const key = 'fiver-to-niner';
+const item = { give: 'me', five: 'dollars' };
+
 test('main exports a class', t => {
   t.deepEqual(typeof Vanillite, 'function');
+});
+
+test('inserts & gets faster, and stores correctly', async t => {
+  const vanilliteStore = new Vanillite({ name: 'test1' });
+  const localTestStore = localForage.createInstance({ name: 'test2' });
+
+  const localForageTime = await getActionTime(() =>
+    localTestStore.setItem(key, item)
+  );
+  const vanilliteTime = await getActionTime(() =>
+    vanilliteStore.setItem(key, item)
+  );
+
+  // console.log('\n\n', 'store times', vanilliteTime, localForageTime, '\n\n');
+
+  t.true(vanilliteTime < localForageTime);
+
+  const vtime = await getActionTime(() => vanilliteStore.getItem(key));
+  const ltime = await getActionTime(() => localTestStore.getItem(key));
+
+  t.true(vtime < ltime);
+
+  // console.log('\n\n', 'get times', vtime, ltime, '\n\n');
+
+  const localStored = await localTestStore.getItem(key);
+  const vanilliteStored = await vanilliteStore.getItem(key);
+
+  return t.deepEqual(vanilliteStored, localStored);
+});
+
+test('must be storing items', async t => {
+  const vanilliteStore = new Vanillite({ name: 'storeTest' });
+  vanilliteStore.setItem(key, item);
+
+  await sleep(10);
+  t.deepEqual(key, vanilliteStore.cacheLog[0]);
+});
+
+test('when storing more than cache limit, it must not return dupes', async t => {
+  const smolStore = new Vanillite({ name: 'smolCache', maxCacheItems: 40 });
+
+  [...Array(50).keys()].forEach(index =>
+    smolStore.setItem(`${index}-storetest`, item)
+  ); // don't wait on the inserts
+
+  const inFlightKeys = await smolStore.keys();
+  t.deepEqual(inFlightKeys.length, [...new Set(inFlightKeys)].length);
+  // used because it's a cheap unique
+
+  await sleep(100); // wait for store events to calm
+
+  const finalKeys = await smolStore.keys();
+  t.deepEqual(50, finalKeys.length);
+  t.deepEqual(40, smolStore.cacheLog.length);
+});
+
+test('iterator functions identically, even on small cache sizes with incomplete writes', async t => {
+  const smolIterate = new Vanillite({ name: 'smallen-it', maxCacheItems: 40 });
+
+  [...Array(50).keys()].forEach(index =>
+    smolIterate.setItem(`${index}-storetest`, item)
+  ); // don't wait on the inserts
+
+  const accumulator = [];
+  await smolIterate.iterate((value, id, index) => {
+    accumulator.push({ id, value, index });
+  });
+
+  t.deepEqual(
+    accumulator,
+    [...Array(50).keys()].map(index => ({
+      id: `${index}-storetest`,
+      value: item,
+      index,
+    }))
+  );
 });
--- index.ts
+++ index.ts
@@ -1,73 +1,61 @@
 import * as localForage from 'localforage';
 
-type Cache = {
-  [index: string]: any;
-};
+const unique = (anyArray: Array<any>) =>
+  anyArray.filter((value, index, self) => self.indexOf(value) === index);
 
 type CacheSettings = {
-  lastPurge: number;
   maxCacheItems: number;
-  maxCacheTimeout: number;
 };
 
 type VanilliteOptions = {
   maxCacheItems?: number;
-  maxCacheTimeout?: number;
 } & LocalForageOptions;
 
-class Vanillite {
+class Vanillite<ItemType> {
   store: LocalForage;
 
-  cache: Cache;
+  /* key:value store of items held in memory up to items limits */
+  cache: { [index: string]: ItemType };
+
+  /* log of ids of successfully stored items existing in cache */
+  cacheLog: Array<string>;
 
   cacheSettings: CacheSettings;
 
   constructor(options: VanilliteOptions) {
-    const { maxCacheItems = 1000, maxCacheTimeout = 1000, ...rest } = options;
-
-    this.cacheSettings = {
-      maxCacheItems,
-      maxCacheTimeout,
-      lastPurge: Date.now(),
-    };
+    const { maxCacheItems = 1000, ...rest } = options;
 
     this.store = localForage.createInstance(rest);
 
     this.cache = {};
+    this.cacheLog = [];
+
+    this.cacheSettings = {
+      maxCacheItems,
+    };
   }
 
-  getItem = (key: string): Promise<unknown> => {
+  getItem = (key: string): Promise<ItemType> => {
     if (this.cache[key]) return Promise.resolve(this.cache[key]);
     return this.store.getItem(key);
   };
 
-  setItem = (key: string, value: unknown): Promise<unknown> => {
-    this.cache[key] = value;
-
-    const atCacheLimit =
-      Object.keys(this.cache).length === this.cacheSettings.maxCacheItems;
-    const atCacheTimeLimit =
-      Date.now() >
-      this.cacheSettings.lastPurge + this.cacheSettings.maxCacheTimeout;
-
-    if (atCacheLimit || atCacheTimeLimit) this.stashCache();
+  setItem = (key: string, value: ItemType): Promise<ItemType> => {
+    this.storeItem(key, value); // executes nonblocking
 
     return Promise.resolve(value);
   };
 
   removeItem = (key: string): Promise<void> => {
     delete this.cache[key];
+    this.cacheLog = this.cacheLog.filter(cacheKey => cacheKey === key);
     return this.store.removeItem(key);
   };
 
   clear = (): Promise<void> => {
     this.cache = {};
+    this.cacheLog = [];
     return this.store.clear();
-  };
-
-  length = async (): Promise<number> => {
-    const storeLength = await this.store.length();
-    return storeLength + Object.keys(this.cache).length;
   };
 
   key = async (index: number): Promise<string> => {
@@ -78,20 +66,68 @@ class Vanillite {
 
   keys = async (): Promise<string[]> => {
     const storeKeys = await this.store.keys();
-    return [...storeKeys, ...Object.keys(this.cache)];
+    return unique([...storeKeys, ...Object.keys(this.cache)]);
   };
 
-  private storeCacheItem = async (key, value) => {
+  length = async (): Promise<number> => {
+    const keys = await this.keys();
+    return keys.length;
+  };
+
+  iterate = async (
+    iterationFunction: (value: ItemType, key: string, index: number) => void,
+    finalCallback: () => void | undefined = () => {}
+  ) => {
+    const cacheKeys = Object.keys(this.cache);
+
+    cacheKeys.forEach((key, index) => {
+      iterationFunction(this.cache[key], key, index);
+    });
+
+    let workingIndex = cacheKeys.length;
+    await this.store.iterate((value, key) => {
+      if (cacheKeys.includes(key)) return; // FILTER DUPES
+
+      workingIndex += 1;
+      iterationFunction(value as ItemType, key, workingIndex);
+    });
+
+    return Promise.resolve(finalCallback());
+  };
+
+  createInstance = (options: VanilliteOptions & LocalForageOptions) => {
+    return new Vanillite(options);
+  };
+
+  dropInstance = async () => {
+    await this.clear();
+    return this.store.dropInstance();
+  };
+
+  private storeItem = async (key, value) => {
+    this.cache[key] = value;
+
     await this.store.setItem(key, value);
-    delete this.cache[key];
+    this.cacheLog.push(key);
+    this.downsizeCache();
   };
 
-  private stashCache = () => {
-    Promise.all(
-      Object.entries(this.cache).map(([key, value]) =>
-        this.storeCacheItem(key, value)
-      )
-    );
+  private downsizeCache = () => {
+    const exceededCache =
+      this.cacheLog.length - this.cacheSettings.maxCacheItems;
+
+    if (exceededCache >= 5) this.unloadOldest(exceededCache);
+    // this is to reduce the number of calls, cache size should hardly be affected by +5
+  };
+
+  private unloadOldest = (toUnload: number) => {
+    const oldestIds = this.cacheLog.slice(0, toUnload);
+    const allowedCache = this.cacheLog.slice(toUnload);
+
+    oldestIds.forEach(oldItemId => {
+      delete this.cache[oldItemId];
+    });
+    this.cacheLog = allowedCache;
   };
 }
 
